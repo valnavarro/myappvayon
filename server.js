1d0460ebbca7eedd18b6fdafb182c86192da7dc5
Valdemir Navarro <vnavarro@vayon.com.br>
2015-11-28 13:09:14 -0200

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+
+// BASE SETUP
+// ======================================
+// CALL THE PACKAGES --------------------
+var express    	= require('express');		// call express
+var app        	= express(); 				// define our app using express
+var path 	   	= require('path');
+
+// set static files location
+// used for requests that our frontend will make
+app.use(express.static(__dirname + '/www'));
+
+app.get('*', function(req, res) {
+	res.sendFile(path.join(__dirname + '/www/index.html'));
+});
+
+// START THE SERVER
+// ====================================
+// start the server
+app.listen(1337);
+console.log('1337 is the magic port!');
+ 
+
+
